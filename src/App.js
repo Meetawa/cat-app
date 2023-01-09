c13227a10b3922f6404769d369e44f179067d5a8
Rahul Saini <113601684+rahulmeetawa@users.noreply.github.com>
2023-01-09 09:38:45 +0530

    error fixed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React,{useEffect,useState} from 'react'
 
 import DropdownMenu, { DropdownItem, DropdownItemGroup } from '@atlaskit/dropdown-menu';
+
 function App() {
-  
   const [users,setUser]=useState([])
   useEffect(()=>{
-    fetch("https://optimizory.atlassian.net/rest/api/3/priority").then((result)=>{
+    AP.request('/rest/api/3/priority').then((result)=>{
       result.json().then((resp)=>{
         setUser(resp)
       })
     }).catch(err=>{
-      console.log(err);
+      console.log(err)
   })
   },[])
-  console.warn(users)
   return (
       <DropdownMenu trigger="Priorities">
         {
